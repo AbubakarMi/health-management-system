b4c372e986a6ad35d70969d5fbb133f2961a5f04
AbubakarMi <abubakarmi131@gmail.com>
2025-08-07 10:34:54 +0000

    Remove .next folder and ignore it

--- .next/static/chunks/src_app_admin_layout_tsx_69538df8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_90956b70._.js",
-  "static/chunks/node_modules_2c69755b._.js"
-],
-    source: "dynamic"
-});
