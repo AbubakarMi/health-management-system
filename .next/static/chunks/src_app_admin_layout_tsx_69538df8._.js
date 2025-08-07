3284b31654071b2dad097a6d7999520321e8d903
AbubakarMi <abubakarmi131@gmail.com>
2025-08-07 09:59:21 +0000

    next

--- .next/static/chunks/src_app_admin_layout_tsx_69538df8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_bef7d95f._.js",
-  "static/chunks/node_modules_9c503024._.js"
-],
-    source: "dynamic"
-});
