6bb83934b07b59fa9ec160f658bc5e20b9f9051d
AbubakarMi <abubakarmi131@gmail.com>
2025-08-08 10:29:30 +0000

    allow to select year in the calender to make it fast

--- src/app/doctor/external-records/page.tsx
+++ src/app/doctor/external-records/page.tsx
@@ -131,11 +131,14 @@ export default function ExternalRecordsPage() {
                                         </PopoverTrigger>
                                         <PopoverContent className="w-auto p-0" align="start">
                                             <Calendar
-                                            mode="single"
-                                            selected={field.value}
-                                            onSelect={field.onChange}
-                                            disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                                            initialFocus
+                                                mode="single"
+                                                captionLayout="dropdown-buttons"
+                                                fromYear={1920}
+                                                toYear={new Date().getFullYear()}
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                                                initialFocus
                                             />
                                         </PopoverContent>
                                     </Popover>
