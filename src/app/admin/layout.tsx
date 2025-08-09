88bfd5b421a33cc6fa3b367136e007136a7c9f0d
AbubakarMi <abubakarmi131@gmail.com>
2025-08-09 12:32:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { AppSidebar } from "@/components/shared/app-sidebar";
 import { AppHeader } from "@/components/shared/header";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
