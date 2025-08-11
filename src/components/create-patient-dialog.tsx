a0eb65ea550a0300e5a13c5ab9cc68beed5f7d9d
AbubakarMi <abubakarmi131@gmail.com>
2025-08-11 14:00:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/create-patient-dialog.tsx
+++ src/components/create-patient-dialog.tsx
@@ -241,22 +241,22 @@ export function CreatePatientDialog({ isOpen, onClose, onPatientSaved, importedD
                                                         <FormLabel>Date of Birth</FormLabel>
                                                         <Popover>
                                                             <PopoverTrigger asChild>
-                                                            <FormControl>
-                                                                <Button
-                                                                variant={"outline"}
-                                                                className={cn(
-                                                                    "w-full justify-start text-left font-normal",
-                                                                    !field.value && "text-muted-foreground"
-                                                                )}
-                                                                >
-                                                                <CalendarIcon className="mr-2 h-4 w-4" />
-                                                                {field.value ? (
-                                                                    format(field.value, "PPP")
-                                                                ) : (
-                                                                    <span>Pick a date</span>
-                                                                )}
-                                                                </Button>
-                                                            </FormControl>
+                                                                <FormControl>
+                                                                    <Button
+                                                                    variant={"outline"}
+                                                                    className={cn(
+                                                                        "w-full justify-start text-left font-normal",
+                                                                        !field.value && "text-muted-foreground"
+                                                                    )}
+                                                                    >
+                                                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                                                    {field.value ? (
+                                                                        format(field.value, "PPP")
+                                                                    ) : (
+                                                                        <span>Pick a date</span>
+                                                                    )}
+                                                                    </Button>
+                                                                </FormControl>
                                                             </PopoverTrigger>
                                                             <PopoverContent className="w-auto p-0" align="start">
                                                             <Calendar
